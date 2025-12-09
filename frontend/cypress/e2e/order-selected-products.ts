4927aecdaf20527441de6ae42badf0e618a1b2e0
richardMattsson <mattsson.richard@gmail.com>
2025-12-09 15:18:12 +0100

    test again with tests

--- frontend/cypress/e2e/order-selected-products.ts
+++ frontend/cypress/e2e/order-selected-products.ts
@@ -1,14 +1,14 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
-// import { mockDataProducts } from "../support/commands.ts";
+import { mockDataProducts } from "../support/commands.ts";
 
 // beforeEach(() => {
-//   mockDataProducts();
-//   cy.visit("/");
-//   cy.wait("@products");
+//
 // });
 
 Given("I selected some product and went to the cart page", () => {
+  mockDataProducts();
   cy.visit("/");
+  cy.wait("@products");
   cy.get("[data-cy=product-card]").eq(0).click();
   cy.get("[data-cy=add-to-cart-button]").click();
   cy.get("[data-cy=shopping-cart]").click();
