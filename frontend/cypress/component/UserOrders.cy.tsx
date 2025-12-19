bb5f431dbf0a8cb1f522b6b9d0e8f5efdbf238fa
richardMattsson <mattsson.richard@gmail.com>
2025-12-19 12:17:47 +0100

    TDD UserOrders

--- frontend/cypress/component/UserOrders.cy.tsx
+++ frontend/cypress/component/UserOrders.cy.tsx
@@ -1,31 +1,30 @@
 import type { ProductType } from "../../src/components/ProductContainer.tsx";
 import UserOrders from "../../src/components/UserOrders.tsx";
 
+const orders: ProductType[] = [
+  {
+    id: 1,
+    name: "Fralla Naturell",
+    category: "frallor",
+    description:
+      "Luftig fralla bakad med vetemjöl, vatten och lite smör. Perfekt till frukosten.",
+    price: 8,
+    image: "/images/fralla-naturell.jpg",
+    quantity: 4,
+  },
+  {
+    id: 2,
+    name: "Fralla Sesam",
+    category: "frallor",
+    description: "Mjuk fralla toppad med sesamfrön. Mild smak och krispig yta.",
+    price: 9,
+    image: "/images/products/fralla-sesam.png",
+    quantity: 4,
+  },
+];
+
 describe("UserOrders.cy.jsx", () => {
   it("shows a list of previous orders", () => {
-    const orders: ProductType[] = [
-      {
-        id: 1,
-        name: "Fralla Naturell",
-        category: "frallor",
-        description:
-          "Luftig fralla bakad med vetemjöl, vatten och lite smör. Perfekt till frukosten.",
-        price: 8,
-        image: "/images/fralla-naturell.jpg",
-        quantity: 4,
-      },
-      {
-        id: 2,
-        name: "Fralla Sesam",
-        category: "frallor",
-        description:
-          "Mjuk fralla toppad med sesamfrön. Mild smak och krispig yta.",
-        price: 9,
-        image: "/images/products/fralla-sesam.png",
-        quantity: 4,
-      },
-    ];
-
     cy.mount(<UserOrders orders={orders} />);
 
     cy.get("[data-cy=order-history-list]")
@@ -38,19 +37,7 @@ describe("UserOrders.cy.jsx", () => {
       .should("have.text", "Fralla Sesam");
   });
 
-  it("show previuos orders with a OrderItemComponent", () => {
-    const orders = [
-      {
-        id: 1,
-        name: "Fralla Naturell",
-        category: "frallor",
-        description:
-          "Luftig fralla bakad med vetemjöl, vatten och lite smör. Perfekt till frukosten.",
-        price: 8,
-        image: "/images/fralla-naturell.jpg",
-        quantity: 4,
-      },
-    ];
+  it("show previuos orders with an OrderItemComponent", () => {
     cy.mount(<UserOrders orders={orders} />);
 
     cy.get("[data-cy=order-item]");
--- frontend/src/components/UserOrders.tsx
+++ frontend/src/components/UserOrders.tsx
@@ -8,8 +8,21 @@ export default function UserOrders({ orders }: UserOrdersProps) {
   return (
     <ul data-cy="order-history-list">
       {orders.map((item) => (
-        <li key={item.id}>{item.name}</li>
+        <OrderItem key={item.id} order={item} />
       ))}
     </ul>
+  );
+}
+
+type OrderItemProps = {
+  order: ProductType;
+};
+
+export function OrderItem({ order }: OrderItemProps) {
+  return (
+    <li data-cy="order-item">
+      <img src={order.image} alt={order.name} />
+      <span>{order.name}</span>
+    </li>
   );
 }
