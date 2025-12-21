7e7acb5c41ee9b003317d94ca30bc5d543c5e345
richardMattsson <mattsson.richard@gmail.com>
2025-12-21 09:12:12 +0100

    fix login test with jwt

--- frontend/cypress/e2e/login.cy.ts
+++ frontend/cypress/e2e/login.cy.ts
@@ -1,22 +1,33 @@
 describe("login", () => {
+  it("back up db", () => {
+    cy.backupDatabase();
+  });
+
   it("success, login", () => {
     cy.visit("/#/login");
 
-    cy.get("[data-cy=email-input-login]").type("example@email.com");
+    cy.request("POST", "/api/register", {
+      email: "test@mail.com",
+      password: "password",
+    });
+
+    cy.get("[data-cy=email-input-login]").type("test@mail.com");
     cy.get("[data-cy=password-input-login]").type("password");
 
     cy.get("[data-cy=submit-login]").click();
 
     cy.location().should((location) =>
-      expect(location.hash).to.eq("#/account/1")
+      expect(location.hash).to.eq("#/account")
     );
-    cy.contains("Tidigare ordrar");
   });
 
-  it.only("redirects to login if fail", () => {
+  it("redirects to login if fail", () => {
     cy.visit("/#/account");
     cy.location().should((location) => {
       expect(location.hash).to.eq("#/login");
     });
+  });
+  it("restore db", () => {
+    cy.restoreDatabase();
   });
 });
